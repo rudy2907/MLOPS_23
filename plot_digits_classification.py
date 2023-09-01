1af107420d5eefa47ce1ada6ee9d07e0ec44a576
rudy2907 <rudradutt2907@gmail.com>
2023-09-01 20:39:47 +0530

    made the changes to the function name since had done the code befor and stored in MLOPS23 that can be found on profile but some error was comming therfore created this again and used the new code that i had created with dev and prediction again therfore in this made changes to name

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 
 # Define the function for splitting data into train, dev, and test sets
-def split_train_dev_test(X, y, test_size, dev_size):
+def Split_Train_Dev_Test(X, y, test_size, dev_size):
     X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=(test_size + dev_size), shuffle=False)
     X_dev, X_test, y_dev, y_test = train_test_split(X_temp, y_temp, test_size=(dev_size / (test_size + dev_size)),
                                                     shuffle=False)
@@ -12,7 +12,7 @@ def split_train_dev_test(X, y, test_size, dev_size):
 
 
 # Define the function for predicting and evaluating the model
-def predict_and_eval(model, X_test, y_test):
+def Predict_and_Eval(model, X_test, y_test):
     predicted = model.predict(X_test)
     print(f"Classification report for classifier {model}:\n{metrics.classification_report(y_test, predicted)}\n")
 
@@ -38,7 +38,7 @@ digits = datasets.load_digits()
 data = digits.images.reshape((len(digits.images), -1))
 
 # Split data into train, dev, and test sets
-X_train, X_dev, X_test, y_train, y_dev, y_test = split_train_dev_test(data, digits.target, test_size=0.5, dev_size=0.2)
+X_train, X_dev, X_test, y_train, y_dev, y_test = Split_Train_Dev_Test(data, digits.target, test_size=0.5, dev_size=0.2)
 
 # Create a classifier: a support vector classifier
 clf = svm.SVC(gamma=0.001)
@@ -47,7 +47,7 @@ clf = svm.SVC(gamma=0.001)
 clf.fit(X_train, y_train)
 
 # Predict and evaluate the classifier on the dev set
-predict_and_eval(clf, X_dev, y_dev)
+Predict_and_Eval(clf, X_dev, y_dev)
 
 # Visualize the dev set predictions
 _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
