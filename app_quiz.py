8697e59fb6ed237c91582be8554966eab69d7adc
rudy2907 <rudradutt2907@gmail.com>
2023-11-19 14:44:25 +0530

    added the app.py

---
+++ app_quiz.py
@@ -0,0 +1,26 @@
+# Assuming your test file is named test_app.py
+# Add the following to test_app.py
+
+import json
+from api.app import app
+
+def test_post_predict():
+    # Replace the following line with the actual data from your dataset
+    sample_data = {
+        "sample_0": 0,
+        "sample_1": 1,
+        "sample_2": 2,
+        "sample_3": 3,
+        "sample_4": 4,
+        "sample_5": 5,
+        "sample_6": 6,
+        "sample_7": 7,
+        "sample_8": 8,
+        "sample_9": 9,
+    }
+
+    for suffix, expected_digit in sample_data.items():
+        response = app.test_client().post("/", json={"suffix": suffix})
+        assert response.status_code == 200
+        assert response.get_json()["predicted_digit"] == expected_digit
+
